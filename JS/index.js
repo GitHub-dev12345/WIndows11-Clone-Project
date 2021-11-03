6ba67f89fda4e6035ca6c5afe4d4348bf92d8e1e
Dev Rathore <devrathore986@gmail.com>
2021-11-03 12:29:34 +0530

    Original File of Windows 11 Clone

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+console.log("Windows 11 here !");
+
+let taskbar = document.getElementsByClassName("taskbar")[0]
+let startmenu = document.getElementsByClassName("startmenu")[0]
+
+taskbar.addEventListener("click", ()=>{
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-655px"
+    }
+    else{
+        startmenu.style.bottom = "50px"
+    }
+})
